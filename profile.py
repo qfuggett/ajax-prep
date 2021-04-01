e310356ade1fa3f4717dc6a22fb27791a3e8e2c7
qfuggett <qfuggett@gmail.com>
2021-04-01 14:01:01 +0000

    simple flask application that returns results from a profile form

---
+++ profile.py
@@ -0,0 +1,32 @@
+import random
+
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Show index page."""
+    
+    return render_template("index.html")
+
+
+@app.route('/profile', methods=['POST'])
+def profile():
+    """Return results from profile form."""
+
+    fullname = request.form['name']
+    occupation = request.form['occupation']
+    age = request.form['age']
+    # TODO: get the values from the rest of the form
+    # Add them to jsonify
+    
+    return jsonify({'fullname': fullname, 'occupation': occupation, 'age': age})
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
